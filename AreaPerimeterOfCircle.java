df374d5cf649789e0dcbe43d635bcb1691a0cab3
Christian Darvin <110832080+christiandarvs@users.noreply.github.com>
2022-09-14 22:58:57 +0800

    Update AreaPerimeterOfCircle.java

--- AreaPerimeterOfCircle.java
+++ AreaPerimeterOfCircle.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Radius: ");
+        double radius = scan.nextDouble();
 
+        final double PI = 3.141592653589793238462643;
+
+        double area = PI * (radius)*(radius);
+        double perimeter = 2 * PI * radius;
+
+        System.out.println("Perimeter is = " +perimeter);
+        System.out.println("Area is = " +area);
+        }
+    }
